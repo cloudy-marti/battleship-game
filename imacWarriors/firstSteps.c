388ad25b61edfe065e0032e48c3cb704dfe6c89e
Cloudy <cloud.kira@gmail.com>
2017-12-27 19:14:16 +0100

    un truc

---
+++ imacWarriors/firstSteps.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct unit {
+    int x; 
+    int y; // x and y = coordinates in the game
+    
+    char type[2];
+    type[0] = w; // w for warrior
+    type[1] = s; // s for servant
+    
+    
+} Unit;
+
+struct world {
+    
+}
